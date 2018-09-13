8a9aac8ce970741644a7e0839c6ea8a06aebbe5f
Laurence Wingo <laurence@cosmicarrows.com>
2018-09-13 08:52:28 -0400

    Initial Commit

---
+++ HelloWorldCK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldCK
+//
+//  Created by Laurence Wingo on 9/13/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
